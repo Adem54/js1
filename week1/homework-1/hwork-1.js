c1250bf2e868b5931098fc387eaa3bfba4cf8787
Adem54 <adem5434e@gmail.com>
2019-05-13 12:54:56 +0200

    Ödev Son

--- week1/homework-1/hwork-1.js
+++ week1/homework-1/hwork-1.js
@@ -3,32 +3,42 @@ let islem2 = "-";
 let islem3 = "*";
 let islem4 = "/";
 
-let a = parseInt(prompt("Bir sayı giriniz: "));
-let b = parseInt(prompt("Bir sayı daha giriniz: "));
+let a = parseInt(prompt("Bir sayı giriniz: ")); //Prompt içerisindeki veri herzaman string old için numaraya çevirdik
+let b = parseInt(prompt("Bir sayı daha giriniz: ")); //Ancak number methodu ile olmadı ama parseInt methodu ile çözüldü
 let c = prompt("Bir işlem türü seçiniz:");
 
 switch ((a, b, c)) {
   case islem1:
     document.write(a + b);
     console.log(a + b);
+    alert(a + b);
     break;
   case islem2:
     if (a > b) {
       document.write(a - b);
+      console.log(a - b);
+      alert(a - b);
     } else {
       document.write(b - a);
+      console.log(b - a);
+      alert(b - a);
     }
 
     break;
   case islem3:
     document.write(a * b);
     console.log(a * b);
+    alert(a * b);
     break;
   case islem4:
     if (a > b) {
       document.write(a / b);
+      console.log(a / b);
+      alert(a / b);
     } else {
       document.write(b / a);
+      console.log(b / a);
+      alert(b / a);
     }
 
     break;
